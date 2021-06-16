2ef3b118c82d308b375c7cc5accaeb1ba705b1a4
nghiavu255 <51023707+nghiavu255@users.noreply.github.com>
2021-06-16 10:39:53 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/tintuc', (req, res) => {
+  res.send('day la trang tin tuc')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
